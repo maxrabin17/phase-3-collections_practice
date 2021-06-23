c8bded54b129145b3ff5cf85415d476d9168b95c
maxrabin <max.rabin17@gmail.com>
2021-06-23 18:16:21 -0400

    commit tree 66b0df50d0113cdae1bc12aa5aee57c58dce2b2f

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(integers)
+    integers.sort do |a, b|
+    a <=> b
+    end
+end
 
+def sort_array_desc(integers)
+    integers.sort do |a, b|
+    b <=> a
+    end
+end
+
+def sort_array_char_count(strings)
+    strings.sort do |a, b|
+    a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def swap_elements_from_to(array, index, destination)
+    array[index], array[destination] = array[destination], array[index]
+    array
+end
+
+def reverse_array(integers)
+    integers.reverse
+end
+
+def kesha_maker(strings)
+    strings.each do |s|
+        s[2] = "$"
+    end
+end
+
+def find_a(strings)
+    strings.find_all do |s|
+        s.start_with? "a" 
+    end
+end
+
+def sum_array(num)
+    num.reduce(:+)
+end
+
+def add_s(array)
+    array.map do |word|
+        if array[1] == word
+            word
+        else
+            word + "s"
+        end
+    end
+end
